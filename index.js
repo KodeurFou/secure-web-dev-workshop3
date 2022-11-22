083ab94c0aff87df4b5d1e091e6d50e16a73855a
KodeurFou <adrien.bonnefon@edu.devinci.fr>
2022-11-22 17:06:56 +0100

    TD secure web 3

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv').config()
 const locationController = require('./locations/locations.controller')
 const app = express()
 const port = 3000
 
 
+app.use(bodyParser.json())
 app.use(locationController)
 
-app.listen(port, () => {
+app.listen(port, async() => {
+	await mongoose.connect(process.env.MONGO_URI)
+	console.log("Connected")
 	console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
+
+})
+
+
+app.get('/', (req, res) => {
+	return res.status(200).send("hello world")
 })
